1bc0daa7930b89bb8b2a016238e222f8b51d3e6e
riya310 <riya1267.cse18@chitkara.edu.in>
2020-11-26 23:16:50 +0530

    react basic

---
+++ src/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata=[
+{
+    imgsrc:"https://i.pinimg.com/originals/67/5e/bc/675ebc2fd210a8bd5362928a51514960.jpg",
+    title:"A Netflix Original Series",
+    sname:"DARK",
+    link:"https://www.netflix.com/title/80100172",
+},
+{
+    imgsrc:"https://assetscdn1.paytm.com/images/catalog/product/H/HO/HOMPOSTERSKART-POST94654E2BFA627/1563105075123_0.jpg?imwidth=320&impolicy=hq",
+title:"A Netflix Original Series",
+sname:"Breaking Bad",
+link:"https://www.netflix.com/title/70143836"
+},
+{
+    imgsrc:"https://media-eng.dhakatribune.com/uploads/2019/04/web-1555672174416.jpg",
+    title:"A Netflix Original Series",
+    sname:"Delhi Crime",
+    link:"https://www.netflix.com/title/81076756"
+},
+{
+    imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSARKTLfcVaFhc8GFJtJ6f3Yds36U4_b-h2EpwMlXdbS5H5bjoRLgnx5pL9_sz_kETbpYY-Tiq-Xe5Fso4XKR6UjPZiMJCY7so&usqp=CAU&ec=45732301",
+    title:"A Netflix Original Series",
+    sname:"Sacred Games",
+    link:"https://www.netflix.com/title/80115328"
+},
+{
+    imgsrc:"https://m.media-amazon.com/images/M/MV5BMTkzNjEzMDEzMF5BMl5BanBnXkFtZTgwMDI0MjE4MjE@._V1_UY1200_CR90,0,630,1200_AL_.jpg",
+    title:"A Netflix Original Series",
+    sname:"Peaky Blinders",
+    link:"https://www.netflix.com/title/80002479"
+}
+{
+    imgsrc:"https://i.pinimg.com/originals/9c/9d/da/9c9dda37ef3df025545621c0fee81659.jpg",
+    title:"ANetflix Original Series",
+    sname:"The Crown",
+    link:"https://www.netflix.com/title/80025678"
+}
+];
+
+export default Sdata;
